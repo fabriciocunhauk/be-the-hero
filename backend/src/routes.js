caf00aa066ba9848c701f7d03e955ace54cb8d5e
Fabricio Cunha <60474481+fabriciocunhauk@users.noreply.github.com>
2020-05-16 02:12:54 +0100

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,12 +8,9 @@ const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 routes.post('/sessions', SessionController.create);
-
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
-
 routes.get('/profile', ProfileController.index);
-
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
