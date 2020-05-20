85e28ea9df7f80a0055b05a7f2a3e44a00da89db
Fabricio Cunha <60474481+fabriciocunhauk@users.noreply.github.com>
2020-05-20 03:52:16 +0100

    mobile app in progress

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello Fabricio!</Text>
-    </View>
+    <Routes />
   );
 }
